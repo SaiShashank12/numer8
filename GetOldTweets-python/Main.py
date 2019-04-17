4c6636c4ccc7311990d018a2a1e304120ed259e6
Sai Shashank Mudliar <34842779+The-crazy-one@users.noreply.github.com>
2019-04-17 19:00:51 +0530

    Update Main.py

--- GetOldTweets-python/Main.py
+++ GetOldTweets-python/Main.py
@@ -13,25 +13,25 @@ def main():
 		print("Text: %s" % t.text)
 		print("Mentions: %s" % t.mentions)
 		print("Hashtags: %s\n" % t.hashtags)
-		print("raw: %s\n" % t)
+		
 
-	# Example 1 - Get tweets by username
+	'''# Example 1 - Get tweets by username
 	tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
 	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
 
-	printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
+	printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)'''
 
 	# Example 2 - Get tweets by query search
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('Uttarkhand floods').setSince("2015-01-01").setUntil("2015-12-31").setMaxTweets(50)
 	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
 
-	printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
+	printTweet("### Example 2 - Get tweets by query search [earthquake Indonesia]", tweet)
 
-	# Example 3 - Get tweets by username and bound dates
+	'''# Example 3 - Get tweets by username and bound dates
 	tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
 	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
 
-	printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
+	printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)'''
 
 if __name__ == '__main__':
 	main()
